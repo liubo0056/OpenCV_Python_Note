38f3a8262b13b09a09694a8369d504ebc8e43a59
liubo0056 <67025638+liubo0056@users.noreply.github.com>
2022-04-10 22:31:42 +0800

    Update 01read_picture.py

--- opencv-py/01read_picture.py
+++ opencv-py/01read_picture.py
@@ -1,10 +1,10 @@
 import cv2
 
-img  = cv2.imread("./images/hdxzd.jpg")
+img  = cv2.imread("./images/hdxzd.jpg") # 读入内存,用变量img存储
 
-# img  = cv2.resize(img,(400,500)) # 设置大小的方法
+# img  = cv2.resize(img,(400,500)) # 绝对大小
 
-img  = cv2.resize(img,(0,0),fx=3.5,fy=3.5) # 另一种设置大小的方法
+img  = cv2.resize(img,(0,0),fx=3.5,fy=3.5) # 相对大小
 
 cv2.imshow("img",img)
 
